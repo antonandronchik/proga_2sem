e64086b51682bb57171171afa2c9cff81fb8a537
antonandronchik <antonandron46@gmail.com>
2017-02-27 16:35:46 +0300

    Update Lab1.cpp

--- Lab1.cpp
+++ Lab1.cpp
@@ -4,103 +4,104 @@
 
 using namespace std;
 
-void FillMass(int *, int);
+int* AllocateMemory(int);
+void FillArray(int *, int);
 void DeleteElement(int *,int *, int, int&);
-void ChangeMass(int *, int *, int, int);
-void DisplayMass(int *, int);
-bool True(int);
+void ChangeArray(int *, int *, int, int);
+void DisplayArray(int *, int);
 void Swap(int&, int&);
 void ChoiceSort(int *, int *, int);
-void EnterMass(int *, int);
-void PrintMass(int *, int);
+void EnterArray(int *, int);
+void PrintArray(int *, int);
 
 
 int main()
 {
-	int n, size = 0;
-	cout << "Plese, enter size of masive \n";
+	int n, length = 0;
+	cout << "Plese, enter length of masive \n";
 	while (true)
 	{
 		cin >> n;
-		if (True(n)) break;
-		else cout << "Please enter positive size of massive\n";
+		if (n > 0) break;
+		else cout << "Please enter positive size of array\n";
 	}
 
-	int *mass = new int [n];
-	FillMass(mass, n);
-	//EnterMass(mass, n);
-	cout << "Your massive\n";
-	DisplayMass(mass, n);
+	int *arr = AllocateMemory(n);
+	FillArray(arr, n);
+	//EnterMass(arr, n);
+	cout << "Your array\n";
+	DisplayArray(arr, n);
 
-	int *mass2 = new int[n];
-	DeleteElement(mass, mass2, n, size);
-	cout << "\nNew massive\n";
-	DisplayMass(mass, n - size);
-	cout << "\nMassive of delete elements\n";
-	DisplayMass(mass2, size);
-
-	int *mass3 = new int[n];
-	ChangeMass(mass, mass3, n, size);
-	ChoiceSort(mass3, mass, n - size);
-	cout << "\nSort massive\n";
-	PrintMass(mass, n - size);
+	int *arr2 = AllocateMemory(n);
+	DeleteElement(arr, arr2, n, length);
+	cout << "\nArray of delete elements\n";
+	DisplayArray(arr2, length);
+	cout << "\nNew array\n";
+	DisplayArray(arr, n - length);
+	
+	int *arr3 = AllocateMemory(n);
+	ChangeArray(arr, arr3, n, length);
+	ChoiceSort(arr3, arr, n - length);
+	cout << "\nSort array\n";
+	PrintArray(arr, n - length);
 	cout << endl;
-	delete[] mass3;
-	delete[] mass2;
-	delete[] mass;
+	delete[] arr3;
+	delete[] arr2;
+	delete[] arr;
 	return 0;
 }
 
-void EnterMass(int *mass, int n)
+int* AllocateMemory(int n)
+{
+	int* arr = new int[n];
+		for (int* p = arr; p < arr + n; p++)
+	       *p = 0;
+	return arr;
+}
+
+void EnterArray(int *arr, int n)
 {
 	for (int i = 0; i < n; i++)
 	{
-		cout << "mass[" << (i + 1) << "] = ";
-		cin >> *(mass + i);
+		cout << "arr[" << (i + 1) << "] = ";
+		cin >> arr[i];
 	}
 }
 
-bool True(int n)
-{
-	if (n > 0)
-		return true;
-	else return false;
-}
-
-void FillMass(int *mass, int n)
+void FillArray(int *arr, int n)
 {
 	srand(time(0));
-	for (int count = 0; count < n; count++)
-		mass[count] = rand() % 100 - 40;
+	for (int i = 0; i < n; i++)
+		arr[i] = rand() % 100 - 40;
 }
 
-void PrintMass(int *mass, int n)
+void PrintArray(int *arr, int n)
 {
 	for (int i = 0; i < n; i++)
 	{
 		bool flag = true;
 		for (int j = i + 1; j < n; j++)
-			if (i - j != 0 && *(mass + i) == *(mass + j)) flag = false;
-		if (flag) cout << *(mass + i) << " ";
+			if (i - j != 0 && arr[i] == arr[j]) flag = false;
+		if (flag) cout << arr[i] << " ";
 	}
 }
 
-void DisplayMass(int *mass,int n)
+void DisplayArray(int *arr,int n)
 {
-	for (int count = 0; count < n; count++)
-		cout << *(mass + count) << " ";
+	for (int i = 0; i < n; i++)
+		cout << arr[i] << " ";
 }
 
-void DeleteElement(int *mass, int *mass2, int n, int& size)
+void DeleteElement(int *arr, int *arr2, int n, int& lenth)
 {
-	for (int count = 0; count < n - 1; count++)
+	for (int i = 0; i < n - 1; i++)
 	{
-		if ((*(mass + count + 1) < 0) && (*(mass + count) > 0))
+		if ((arr[i + 1] < 0) && (arr[i] > 0))
 		{
-			*(mass2 + size) = *(mass + count + 1);
-			size++;
-			for(int i = count + 1; i < n - size; i++)
-			*(mass + i) = *(mass + i + 1);
+			arr2[lenth] = arr[i + 1];
+			lenth++;
+			for(int j = i + 1; j < n - lenth; j++)
+			arr[j] = arr[j + 1];
 		}
 	}
 }
@@ -119,26 +120,26 @@ void Swap(int& a, int& b)
 	b = temp;
 }
 
-void ChoiceSort(int *a, int *b, int n) {
+void ChoiceSort(int *arr2, int *arr, int n) {
 	for (int i = 0; i < n - 1; i++) 
 	{
 		int max = i;
 		for (int j = i + 1; j < n; ++j)
-			if (a[j] > a[max]) max = j;
-		Swap(a[i], a[max]);
-		Swap(b[i], b[max]);
+			if (arr2[j] > arr2[max]) max = j;
+		Swap(arr2[i], arr2[max]);
+		Swap(arr[i], arr[max]);
 	}
 }
 
 
-void ChangeMass(int *mass, int *mass3, int n, int size)
+void ChangeArray(int *arr, int *arr3, int n, int lenth)
 {
 	int copy, number;
-	for (int i = 0; i < n - size; i++)
+	for (int i = 0; i < n - lenth; i++)
 	{
 		int counter = 0;
-		copy = *(mass + i);
-		number = *(mass + i);
+		copy = arr[i];
+		number = arr[i];
 		do
 		{
 			number = fmod(copy, 8);
@@ -146,6 +147,6 @@ void ChangeMass(int *mass, int *mass3, int n, int size)
 			if (number % 2 == 0)
 				counter++;
 		} while (copy);
-		*(mass3 + i) = counter;
+		arr3[i] = counter;
 	}
 }
